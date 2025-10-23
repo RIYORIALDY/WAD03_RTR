053edb01ea11696e00d7631e1948c0eab502ece3
Riyo Rialdy Urda <riorialdy13@gmail.com>
2025-10-23 23:54:41 +0700

    feat: Fix database connection and table not found errors

--- prisma/migrations/20251023100651_init/migration.sql
+++ prisma/migrations/20251023100651_init/migration.sql
@@ -1,26 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `carts` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `products` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "carts";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "products";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "users";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
